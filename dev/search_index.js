375ae436e358081258d5c44d31f767d0728b9dd3
Documenter.jl <documenter@juliadocs.github.io>
2022-11-22 21:32:51 +0000

    build based on 9b4c05c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SVDUpdate","category":"page"},{"location":"#SVDUpdate","page":"Home","title":"SVDUpdate","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SVDUpdate.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SVDUpdate]","category":"page"}]
+}
